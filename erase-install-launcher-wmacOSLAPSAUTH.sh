5b2a8a60a8f781b61c95a52abeefa17ac42554b2
John <jcmbowman@users.noreply.github.com>
2024-02-08 09:49:17 -0500

    Update erase-install-launcher-wmacOSLAPSAUTH.sh
    Update list of forbidden password characters

--- erase-install-launcher-wmacOSLAPSAUTH.sh
+++ erase-install-launcher-wmacOSLAPSAUTH.sh
@@ -37,9 +37,9 @@ Parameters without values are ok to put in a single Parameter field in Jamf.
 e.g. this is OK: "--erase --reinstall --confirm"
 
 ADDITIONAL KNOWN LIMITATION
-You MUST exclude the characters "[", "]", "=", "\", "!", """, and "$" from your macOSLAPS 
-password to prevent the password from hanging up the erase-install script. This
-list is still tentative - any feedback is appreciated.  
+You MUST exclude the characters "[", "]", "=", "\", "!", """, ":", and "$" from your 
+macOSLAPS password to prevent the password from hanging up the erase-install script. 
+This list is still tentative - any feedback is appreciated.  
 
 DOC
 
